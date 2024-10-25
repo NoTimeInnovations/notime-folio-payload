b4b835a50640f08235ac2bb595cf5000034fe23e
NotimeInnovations <projects.notime@gmail.com>
2024-10-25 18:37:56 +0530

    changed roadmap_id , topic_id to array

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -156,11 +156,13 @@ const Users: CollectionConfig = {
         {
           name: 'roadmap_id',
           type: 'text',
+          hasMany: true,
           required: true,
         },
         {
           name: 'topic_id',
           type: 'text',
+          hasMany: true,
           required: true,
         },
       ],
